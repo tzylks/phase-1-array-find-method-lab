e58dcc5c1c294abf0894d35dae0c52c366a47a7c
TrevorZylks <trevorzylks@gmail.com>
2021-06-30 10:33:09 -0700

    commit tree 4df4631eec11bfbec35bfac957d4d2769343411e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(record){
+    const winList = record.find(obj => obj.result === 'W'); //return an element if condition met
+    //if (record.find(obj => obj.result === 'W') ) 
+    if (winList) { //if winList is "true" meaning it has an element, then return the year; if its "false" meaning it's empty, then undefined
+        return winList.year
+    } else {return undefined}
+    // const winList = record.find(obj => obj.result === 'W');
+    //return winList.year; //passes 1st part
+    //return winList; ==> passes 2nd part (undefined)
+}
